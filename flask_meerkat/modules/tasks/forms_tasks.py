fa29eff4495e6869313c89b74154e7446193c8a2
LaerryLaessig <laerrylaessig@gmail.com>
2021-10-24 17:57:12 +0200

    create first task page

---
+++ flask_meerkat/modules/tasks/forms_tasks.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FieldList, FormField, BooleanField, SelectField
+from wtforms.validators import DataRequired
+
+
+class SubTaskForm(FlaskForm):
+    subtask = StringField('Subtask', validators=[DataRequired()])
+    status = BooleanField('Status')
+
+
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    reviser = SelectField('Reviser', choices=[('', ''), ('1', 'enno'), ('2', 'resi')])
+    subtasks = FieldList(FormField(SubTaskForm), min_entries=1, max_entries=25)
+    new_subtask = SubmitField('+', render_kw={'formnovalidate': True})
+    remove_last_subtask = SubmitField('-',  render_kw={'formnovalidate': True})
+    submit = SubmitField('Save')
+
+
+
--- flask_meerkat/modules/tasks/routes_tasks.py
+++ flask_meerkat/modules/tasks/routes_tasks.py
@@ -3,8 +3,22 @@ from flask_meerkat import app
 from flask import render_template
 from flask_login import login_required
 
+from flask_meerkat.modules.tasks.forms_tasks import TaskForm
+
 
 @app.route('/tasks', methods=['GET', 'POST'])
 @login_required
 def tasks():
+
     return render_template('tasks/tasks.html')
+
+
+@app.route('/task', methods=['GET', 'POST'])
+@login_required
+def create_task():
+    form = TaskForm()
+    if form.new_subtask.data:
+        form.subtasks.append_entry()
+    if form.remove_last_subtask.data:
+        form.subtasks.pop_entry()
+    return render_template('tasks/upsert_task.html', form=form)

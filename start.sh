f739c4f9d0095289911ccb83aeb15acf55bdbf1f
LaerryLaessig <laerrylaessig@gmail.com>
2022-05-15 08:02:06 +0200

    add start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "activate virtual venv"
+. venv/bin/activate
+
+echo "set admin user and pw"
+export SECRET_KEY = ''
+export MAIL_USERNAME = ''
+export MAIL_PASSWORD = ''
+export MAIL_DEFAULT_SENDER = ''
+
+echo 'start server'
+python app.py
+
+echo "deactivate venv"
+deactivate

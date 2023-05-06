db214d27b91aabeebd6bcdbaa8d0f3c2f73d6d8d
laerrylaessig <laerrylaessig@gmail.com>
2023-05-06 11:44:12 +0200

    update dependencies

--- flask_meerkat/__init__.py
+++ flask_meerkat/__init__.py
@@ -15,29 +15,30 @@ IMAGE_FOLDER = os.path.join('flask_quiz', 'static', 'images')
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meerkat.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config.update(MAIL_SERVER='smtp.gmail.com',
-                  MAIL_PORT=465,
-                  MAIL_USE_SSL=True,
-                  MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER'),
-                  MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
-                  MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'))
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'signin'
-login_manager.login_message_category = 'info'
+with app.app_context():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meerkat.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config.update(MAIL_SERVER='smtp.gmail.com',
+                      MAIL_PORT=465,
+                      MAIL_USE_SSL=True,
+                      MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER'),
+                      MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
+                      MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'))
+    bcrypt = Bcrypt(app)
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'signin'
+    login_manager.login_message_category = 'info'
 
-mail = Mail(app)
-fa = FontAwesome(app)
+    mail = Mail(app)
+    fa = FontAwesome(app)
 
-Bootstrap(app)
+    Bootstrap(app)
 
-db = SQLAlchemy(app)
+    db = SQLAlchemy(app)
 
-import flask_meerkat.database
-from flask_meerkat import models
-import flask_meerkat.modules
+    import flask_meerkat.database
+    from flask_meerkat import models
+    import flask_meerkat.modules
 
-database.create_database()
+    database.create_database()
--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ echo "activate virtual venv"
 . venv/bin/activate
 
 echo "set admin user and pw"
-export SECRET_KEY = 'secret_key'
-export MAIL_USERNAME = ''
-export MAIL_PASSWORD = ''
-export MAIL_DEFAULT_SENDER = ''
-export SERVER_DOMAIN = ''
+export SECRET_KEY='secret_key'
+export MAIL_USERNAME=''
+export MAIL_PASSWORD=''
+export MAIL_DEFAULT_SENDER=''
+export SERVER_DOMAIN=''
 
 echo 'start server'
 python app.py

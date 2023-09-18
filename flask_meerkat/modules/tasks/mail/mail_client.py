786c615f5615abc775ab6933d7c59729dc70120c
laerrylaessig <laerrylaessig@gmail.com>
2023-09-18 09:54:49 +0200

    add more Information in info mail

--- flask_meerkat/modules/tasks/mail/mail_client.py
+++ flask_meerkat/modules/tasks/mail/mail_client.py
@@ -3,12 +3,20 @@ import logging
 from flask_meerkat import mail, SERVER_DOMAIN
 
 
-def send_new_task_mail(email):
+def send_new_task_mail(email, data):
     try:
         mail.send_message(
-                recipients=[email],
-                subject='New Task',
-                body=f'Hi,\nyou have got a new task.\n Visit {SERVER_DOMAIN} to complete your task.'
-            )
+            recipients=[email],
+            subject='New Task',
+            body=create_body(data=data)
+        )
     except AssertionError as e:
         logging.error(f'Failed to send mail: {e}')
+
+
+def create_body(data):
+    body = f'Hi,\nyou have got a new task.\n Visit {SERVER_DOMAIN} to complete your task.\n'
+    body += f'{data["title"]}:\n'
+    for s in data['subtasks']:
+        body += f'- {s["subtask"]}\n'
+    return body
